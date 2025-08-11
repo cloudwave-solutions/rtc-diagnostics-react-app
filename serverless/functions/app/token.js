926136b6ea3e81be17c2d21c92eadc419b0062a3
Thomas <124110182+thomasblakelycloudwave@users.noreply.github.com>
2025-08-11 20:24:54 +1000

    Update token.js
    fixed

--- serverless/functions/app/token.js
+++ serverless/functions/app/token.js
@@ -2,6 +2,8 @@ exports.handler = function (context, event, callback) {
   const verifyExpiry = require(Runtime.getAssets()['/verify_expiry.js'].path);
   verifyExpiry.handler(context, event, callback);
 
+  const VOICE_IDENTITY = context.VOICE_IDENTITY || 'test_user_' + Math.random().toString(36).substr(2, 8);
+
   const AccessToken = Twilio.jwt.AccessToken;
   const VoiceGrant = AccessToken.VoiceGrant;
 
@@ -9,11 +11,17 @@ exports.handler = function (context, event, callback) {
     outgoingApplicationSid: context.TWIML_APP_SID,
   });
 
-  const token = new AccessToken(context.ACCOUNT_SID, context.API_KEY, context.API_SECRET, {
-    ttl: 60,
-  });
+  const token = new AccessToken(
+    context.ACCOUNT_SID,
+    context.API_KEY,
+    context.API_SECRET,
+    {
+      ttl: 3600,
+      identity: VOICE_IDENTITY,
+    }
+  );
+
   token.addGrant(voiceGrant);
-  token.identity = context.VOICE_IDENTITY;
 
   callback(null, { token: token.toJwt() });
 };
